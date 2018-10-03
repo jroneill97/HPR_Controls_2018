6dc53c03a57373be6d983be7f68f33f528f2a709
ojack123 <43734109+ojack123@users.noreply.github.com>
2018-10-02 21:48:38 -0400

    Add files via upload

--- Test_Scripts.m
+++ Test_Scripts.m
@@ -1,81 +1,115 @@
 clc; %clear all; 
 close all;
 
+%%
 global rho        % air density
-global g       % force of gravity (earth fixed)
-global Cd         % drag coefficient
-global Cl         % lift coefficient
-global area          % area matrix   [front side side]
+global g          % force of gravity (earth fixed)
+
+global T          % set thrust to 0
+global area       % area matrix  [nose side side]
+global body_diam  % body diameter
+global L          % rocket length
+global dcgref     % dcg-dcgref: distance between real and referance center of gravity 
+global dcg        % distance from bottom of rocket to cg
+
 global m          % total mass
-global I          % I matrix      eye([Ix Iy Iz])
-global r_cp2cg_b  % distance from cm to cg
-global dcg       % distance from bottom to cg
-global body_diam
+global I          % I matrix     diag([Ix Iy Iz])
 
-g     = -9.8;
-rho    = 1.225;
-Cd     = 0.295;
-Cl     = 0.5;
-area      = 7.5;
-m      = 4.0;
-L      = 1.5;
-body_diam = 2*0.0508;
+global Cl         % Rolling moment coefficient
+global Cm         % Pitching moment coefficient
+global Cn         % Yawing moment coefficient
+
+global CL         % Coefficient of moment due to roll rate
+global CM         % Coefficient of moment due to pitch rate
+global CN         % Coefficient of moment due to yaw rate
+
+global CA         % Axial force coefficient     
+global CY         % Side force coefficient
+global Lap        % 
+global Lay        % 
+global Lda        % 
+global delta      % angle of fins (?)
+global noseL      % nose cone length
+
+%% set global variables to those from the parameter datasets
+load('generalParameters.mat');
+load('rocketParameters.mat');
+
+g           = generalParameters.g;
+rho         = generalParameters.rho;
+
+T           = rocketParameters.T;
+area        = rocketParameters.area;
+body_diam   = rocketParameters.body_diam;
 body_radius = body_diam/2;
-noseL  = .3;
-I      = diag([m*(1/12)*L^2  m*(1/12)*L^2 m*body_radius^2]);
-r_cp2cg_b = [0; 0; .2];
-dcg = .5;
+dcgref      = rocketParameters.dcgref;
+dcg         = rocketParameters.dcgref;
+m           = rocketParameters.m;
+I           = rocketParameters.I;
+L           = rocketParameters.L;
 
+Cl          = rocketParameters.Cl;
+Cm          = rocketParameters.Cm;
+Cn          = rocketParameters.Cn;
 
+CM          = rocketParameters.CM;
+CN          = rocketParameters.CN;
 
-%% Values that need to be found experimentally
-global Cm 
-global CM 
-global Cn 
-global T % set thrust to 0
-global CA 
-global CY 
-global CN 
-global Lap
-global Lay
-global Lda
-global delta
-global dcgref
+CA          = rocketParameters.CA;     
+CY          = rocketParameters.CY;
+Lap         = rocketParameters.Lap;
+Lay         = rocketParameters.Lay;
+Lda         = rocketParameters.Lda;
+delta       = rocketParameters.delta;
+noseL       = rocketParameters.noseL;
 
-Cm = 0;
-CM = 0;
-Cn = 0;
-CN = 0;
-CY = 0;
-T  = 0; % Thrust 
-CA = 0;
-Lap = 0;
-Lay = 0;
-Lda = 0;
-delta = 0;
-dcgref = dcg;
+%% Calculate State matrix 
+states = zeros(1,12); % initialize state matrix
+% position     - 1:3 - x y z
+% velocity     - 4:6 - u v w
+% ang position - 7:9 - roll pitch yaw
+% ang velocity - 10:12
+
+% Define initial states
+states(4)  = 1;
+
+% Define data rate
+dataRate = 1; % Hz
+
+tArray = [0:1/dataRate:2];
+t = 0;      % initialize t       
+for i = 2:length(tArray)
+    [t_new,states_new] = ode45(@(t,y) updated_rocket_equations_of_motion(t,y),...
+                               [tArray(i-1) tArray(i)],states(end,:));
+    % use indexing instead
+    t = vertcat(t,t_new);
+    states = vertcat(states,states_new);
+    
+    disp((i*100)/length(tArray)); % display percent completion
+end
+
+% clean up workspace a bit
+clearvars -except states t body_radius noseL dcg L dataRate
+
+%% Animating the Rocket
+% ------------------------------------------------------------------------
 %% Draw Rocket
 [xc, yc, zc]    = cylinder([body_radius  0] ,5);    % nose cone
 [xa,  ya,  za ] = cylinder(body_radius      ,5);    % cylinder above CG
 [xb,  yb,  zb ] = cylinder(body_radius      ,5);    % cylinder below CG
 [x  y   z]      = cylinder(.005             ,5);    % axis bars
-%[xs xy] = meshgrid([-2 2],[-2:2]);
 
-h(1) = surface(xc,yc      , (noseL*zc+(L-noseL))        ,'FaceColor','white');%,'EdgeColor','none');
-h(2) = surface(xa,ya      , ((L-noseL - dcg)*za + dcg)  ,'FaceColor','white');%,'EdgeColor','none');
-h(3) = surface(xb,yb      , (dcg*zb)                    ,'FaceColor','white');%,'EdgeColor','none');
-h(4) = surface(z ,x       , (y + dcg)                    ,'FaceColor','blue' ,'EdgeColor', 'none');
-h(5) = surface(x ,z       , (y + dcg)                    ,'FaceColor','green','EdgeColor', 'none');
-h(6) = surface(x ,y       , 2*(z + dcg)                     ,'FaceColor','red'  ,'EdgeColor', 'none');
+% Rocket body
+h(1) = surface(xc,yc      , (noseL*zc+(L-noseL))        ,'FaceColor','white');
+h(2) = surface(xa,ya      , ((L-noseL - dcg)*za + dcg)  ,'FaceColor','white');
+h(3) = surface(xb,yb      , (dcg*zb)                    ,'FaceColor','white');
 
-h(1) = surface((noseL*xc+(L-noseL)),yc      ,zc         ,'FaceColor','white');%,'EdgeColor','none');
-h(2) = surface(((L-noseL - dcg)*za + dcg),ya      , za  ,'FaceColor','white');%,'EdgeColor','none');
-h(3) = surface((dcg*zb),yb      , zb                    ,'FaceColor','white');%,'EdgeColor','none');
-h(4) = surface((y + dcg) ,x       , (y + dcg)                    ,'FaceColor','blue' ,'EdgeColor', 'none');
-h(5) = surface((y + dcg) ,z       , (y + dcg)                    ,'FaceColor','green','EdgeColor', 'none');
-h(6) = surface(2*(z + dcg) ,y       , 2*(z + dcg)                     ,'FaceColor','red'  ,'EdgeColor', 'none');
-clear xc yc zc xa ya za xb yb zb x y z;
+% Axis bars
+h(4) = surface(z ,x       , (y + dcg)                   ,'FaceColor','blue' ,'EdgeColor', 'none');
+h(5) = surface(x ,z       , (y + dcg)                   ,'FaceColor','green','EdgeColor', 'none');
+h(6) = surface(x ,y       , 2*(z + dcg)                 ,'FaceColor','red'  ,'EdgeColor', 'none');
 
+clearvars xc yc zc xa ya za xb yb zb x y z body_radius noseL L;
 %% defining the axis
 
 whitebg([0 .5 .6])
@@ -91,41 +125,26 @@ grid on
 body = hgtransform('Parent',ax);
 set(h,'Parent',body);                % gathers all points in h into a single "parent"
 set(gcf,'Renderer','opengl');
+clear h;
 
 view([0 0]);
 drawnow;
-
-%% Calculate State matrix 
-states = zeros(1,12); % initialize states
-states(6) = 2;
-states(7) = 0;
-dataRate = 1; % Hz
-tArray = [0:1/dataRate:5];
-t = 0;      % initialize t       
-for i = 2:length(tArray)
-    [t_new,states_new] = ode45(@(t,y) updated_rocket_equations_of_motion(t,y),...
-                               [tArray(i-1) tArray(i)],states(end,:));
-    % use indexing instead
-    t = vertcat(t,t_new);
-    states = vertcat(states,states_new);
-    disp((i*100)/length(tArray)); % percent completion
-end
 
 %% Animate the rocket trajectory
 pos_i    = states(:,1:3);
 vel_i    = states(:,4:6);
 ang_i    = states(:,7:9);
 angVel_i = states(:,10:12);
+clear states
 
 for i = 2:length(t)
-    pause(.01)
+    pause((t(i)-t(i-1))/dataRate)
     set(body,'Matrix',makehgtform('translate',pos_i(i,:)'+[0;0;dcg],...
                                   'zrotate'  ,ang_i(i,3)',...
                                   'yrotate'  ,ang_i(i,2)',...
                                   'xrotate'  ,ang_i(i,1)',...
                                   'translate',-(pos_i(i,:)'+[0;0;dcg]),...
                                   'translate',pos_i(i,:)'));
-    %surface(xs, xy,[0 0],'FaceColor','green' ,'EdgeColor', 'none');
     drawnow
 end
 
--- updated_rocket_equations_of_motion.m
+++ updated_rocket_equations_of_motion.m
@@ -1,94 +1,84 @@
 function states_i_dot = rocket_equations_of_motion(t,y)
 
+%% define global variables
 global rho        % air density
-global g       % force of gravity (earth fixed)
-%global Cd        % drag coefficient
-%global Cl        % lift coefficient
-global area          % area matrix   [nose side side]
+global g          % force of gravity (earth fixed)
+
+global T          % set thrust to 0
+global area       % area matrix  [nose side side]
+global body_diam  % body diameter
+global dcgref     % distance from something to something 
+global dcg        % distance from bottom of rocket to cg
+
 global m          % total mass
-global I          % I matrix      [Ix Iy Iz]
-%global r_cp2cg_b  % distance from cm to cg
-global body_diam
-global dcg       % distance from bottom to cg
+global I          % I matrix     diag([Ix Iy Iz])
+global L
 
+global Cl         % Rolling moment coefficient
+global Cm         % Pitching moment coefficient
+global Cn         % Yawing moment coefficient
 
-% Values that need to be found experimentally
-global Cm 
-global CM 
-global Cn 
-global T % set thrust to 0
-global CA 
-global CY 
-global CN 
+global CL         % Coefficient of moment due to roll rate
+global CM         % Coefficient of moment due to pitch rate
+global CN         % Coefficient of moment due to yaw rate
+
+global CA         
+global CY  
 global Lap
 global Lay
 global Lda
 global delta
-global dcgref
 
-%% states
-  states_i = y;
-%  pos_i  = states_i(1:3);
-  vel_i  = states_i(4:6);
-  ang_i  = states_i(7:9); % (roll pitch yaw)
-  angV_i = states_i(10:12);
+%% inertial states
+states_i = y;
 
-%% Moment of Inertia Tenareaor
-% I=[Ix 0 Ixz;...
-%     0 Iy Iyz;...
-%     Ixz Iyz Iz];
+pos_i  = states_i(1:3);
+vel_i  = states_i(4:6);
+ang_i  = states_i(7:9); % (roll pitch yaw)
+angV_i = states_i(10:12);
 
 %% Calculate Angle of Attack
-  yaw   = ang_i(3);
-  pitch = ang_i(2);
-  roll  = ang_i(1);
-  
-  alpha = atan2(vel_i(3),vel_i(1));
-  beta  = asin(vel_i(1)/norm(vel_i));
-    
-  ap    = atan2(vel_i(3),vel_i(1));
-  ay    = atan2(vel_i(2),vel_i(1));
-  
-  R=angV_i(3);
-  Q=angV_i(2);
-  P=angV_i(1);
+yaw   = ang_i(3);
+pitch = ang_i(2);
+roll  = ang_i(1); % rotating around length of rocket
 
+alpha = atan2(vel_i(3),vel_i(1));
+beta  = asin(vel_i(1)/norm(vel_i));
+
+ap    = atan2(vel_i(3),vel_i(1));
+ay    = atan2(vel_i(2),vel_i(1));
+
+%% Body-fixed angular velocity (P,Q,R)
+rotationMat = [1  0         -sin(pitch);
+               0  cos(roll)  cos(pitch)*sin(roll);
+               0 -sin(roll)  cos(pitch)*cos(roll)];
+angVel_b = rotationMat*angV_i;
+
+P = angVel_b(1);
+Q = angVel_b(2);
+R = angVel_b(3);
 %% Aerodynamic coefficients
-%Cd =  1.28*sin(norm(AOA)); 
-%Cl =  2*pi*alpha;
-%Cy =  2*pi*beta;
 q  =  0.5*rho*norm(vel_i)^2;  % dynamic pressure
 
 Clap = Lap/(q*area*body_diam); % slope of rolling moment due to pitch
 Clay = Lay/(q*area*body_diam); 
 Clda = Lda/(q*area*body_diam);
-%Cm= pitching moment coefficient
-%CM= rate of change of pitching moment
-%Cn= yawing moment coefficient
-%CN= rate of change of yawing moment
-%CY= side force coefficient
-%dcg-dcgref= distance between real and referance center of gravity
                  
 %% Moments due to Aerodynamics
-L=q*area*body_diam*(Clap*ap+Clay*ay+Clda*delta);
-M=q*area*body_diam*((Cm+CM*(Q*body_diam)./(2*norm(vel_i)))+CM*((dcg-dcgref)/body_diam));
-N=q*area*body_diam*((Cn+CN*(R*body_diam)./(2*norm(vel_i)))+CY*((dcg-dcgref)/body_diam));
- 
-%% Forces due to Aerodynamics
+L_b = Cl*q*area*body_diam;  %q*area*body_diam*(Clap*ap+Clay*ay+Clda*delta);
+M_b = Cm*q*area*L;          %q*area*body_diam*((Cm+CM*(Q*body_diam)./(2*norm(vel_i)))+CM*((dcg-dcgref)/body_diam));
+N_b = Cn*q*area*body_diam;  %q*area*body_diam*((Cn+CN*(R*body_diam)./(2*norm(vel_i)))+CY*((dcg-dcgref)/body_diam));
 
-%alpha=atan(vel_i(3)/vel_i(1));
-%beta=atan(vel_i(2)/vel_i(1));
+Mom_b = [L_b;M_b;N_b];
 
-Fw = m*g*[-sin(pitch);
-          sin(roll)*cos(pitch);
-          cos(roll)*cos(pitch)]; % Total applied force on the rocket
-Fa = -q *[CA+(alpha^2 + beta^2);
-          CN*beta;
-          CN*alpha            ]; % Total aerodynamic force
+%% Total translational force on rocket
+Fx_b = T - m*g*sin(pitch)           - CA*q*area; % Total force in x direction (body)
+Fy_b =     m*g*sin(roll)*cos(pitch) - CY*q*area; % Total force in y direction (body)
+Fz_b =     m*g*cos(roll)*cos(pitch) - CN*q*area; % Total force in z direction (body)
 
-F = Fw;% + Fa
+F_b  = [Fx_b;Fy_b;Fz_b]; % body-fixed translational force on rocket
 
-%% Equations of Motion
+%% Rotation matrices to convert body-fixed to inertial frames
 A=[cos(pitch)*cos(yaw) sin(roll)*sin(pitch)*cos(yaw)-cos(roll)*sin(yaw) cos(roll)*sin(pitch)*cos(yaw)+sin(roll)*sin(yaw);...
    cos(pitch)*cos(yaw) sin(roll)*sin(pitch)*cos(yaw)+cos(roll)*sin(yaw) cos(roll)*sin(pitch)*cos(yaw)*sin(roll)*sin(yaw);...
    -sin(pitch) sin(roll)*cos(pitch) cos(roll)*cos(pitch)];
@@ -101,12 +91,10 @@ C=[0 -R  Q;...
    R  0 -P;...
   -Q  P  0];
 
-pos_i_dot=A*vel_i; %body fixed velocity to ground fixed velocity
-
-ang_i_dot=B*angV_i; %body fixed ang velocity to ground fixed ang velocity
-
-vel_i_dot=(1/m)*F-C*vel_i; %Newton's law in non inertial body frame
-
-angV_i_dot=I\([L;M;N]-C*I*angV_i); %Newton's law rotational in non inertial body frame 
+%% Create time derivative of state matrix
+pos_i_dot=A*vel_i;                 % inertial position
+ang_i_dot=B*angVel_b;                % inertial angular velocity
+vel_i_dot=(F_b/m)-C*vel_i;       % inertial acceleration (transport theorem)
+angV_i_dot=I\(Mom_b-C*I*angVel_b); % inertial angular acceleration
 
 states_i_dot = [pos_i_dot; vel_i_dot; ang_i_dot; angV_i_dot];
