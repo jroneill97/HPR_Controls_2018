85c51f19645f4695d59e064617a0a6709b2d661b
ojack123 <ojack123@gmail.com>
2018-10-21 13:33:58 -0400

    Animation is fixed and quaternions are all correct

--- AnimateRocket.m
+++ AnimateRocket.m
@@ -1,11 +1,12 @@
 function AnimateRocket(tspan,states,stepSize,rocket)
+
+%% Draw the rocket body
 radius = rocket.body_diam / 2;
 % Draw the rocket and axis bars
 [xc, yc, zc]    = cylinder([radius  0]  ,5);    % nose cone
 [xa,  ya,  za ] = cylinder( radius      ,5);    % cylinder above CG
 [xb,  yb,  zb ] = cylinder( radius      ,5);    % cylinder below CG
 [x,   y,   z  ] = cylinder( 0.005                   ,5);    % axis bars
-
 % Rocket body
 h(1) = surface(xc,yc , (rocket.noseL*zc+(rocket.L-rocket.noseL))        ,'FaceColor','white');
 h(2) = surface(xa,ya , ((rocket.L-rocket.noseL - rocket.dcg)*za + rocket.dcg)  ,'FaceColor','white');
@@ -16,55 +17,34 @@ h(5) = surface(x ,z  ,     (y + rocket.dcg) ,'FaceColor','green','EdgeColor','no
 h(6) = surface(x ,y  , 1.5*(z + rocket.dcg) ,'FaceColor','red'  ,'EdgeColor','none');
 
 
-grid on
-ax = axes('XLim',[-5 5],'YLim',[-5 5],'ZLim',[0 max(states(:,3))+10]);
-axis equal
-box on
 axis off
-ax.Clipping = 'off';
-ax.XTick = [];
-ax.YTick = [];
-ax.ZTick = [];
-
+ax = axes('XLim',[min(states(:,1))-1 max(states(:,1))+1],...
+          'YLim',[min(states(:,2))-1 max(states(:,2))+1],...
+          'ZLim',[0  max(states(:,3))+1]);
+grid on
 % Create rocket body from h
 body = hgtransform('Parent',ax);
 set(h,'Parent',body);                % gathers all points in h into a single "parent"
 set(gcf,'Renderer','opengl');
 
-% Animate the rocket trajectory
+%% Interprit translational and angular position from states matrix
+% Translational position
 pos_i    = states(:,1:3);
-[yaw, pitch, roll] = quat2angle(circshift(states(:,7:10),1));
-angPos_i           = [roll pitch yaw]; % angular position expressed in Euler angles
 
-% Euler axis angle and unit vector
+% Euler axis angle (theta) and unit vector (u)
 theta = 2*acos(states(:,10));         % principal angle
-u     = states(:,7:9)./sin(theta/2); % euler axis of rotation
+u     = states(:,7:9)./sin(theta/2);  % euler axis of rotation
+% [yaw, pitch, roll] = quat2angle(circshift(states(:,7:10),1))
+% ang_i           = [roll pitch yaw]; % angular position expressed in Euler angles
 
-
+%% Animate the rocket flight
 view([0 0]);
 for i = 2:length(tspan) 
-    %pause((tspan(i)-tspan(i-1))*stepSize)
-
-%     set(body,'Matrix',makehgtform('translate',pos_i(i-1,:)'+[0;0;rocket.dcg],...
-%                                   'axisrotate',[u(i,1), u(i,2), u(i,3)],theta(i),...
-%                                   'translate',-(pos_i(i-1,:)'-[0;0;rocket.dcg]),...
-%                                   'translate',pos_i(i-1,:)'));
-    set(body,'Matrix',makehgtform('zrotate',angPos_i(i-1,3)),...
-                      makehgtform('yrotate',angPos_i(i-1,2)),...
-                      makehgtform('xrotate',angPos_i(i-1,1)));
+    pause((tspan(i)-tspan(i-1))*stepSize)
+    set(body,'Matrix',makehgtform('translate',pos_i(i-1,:)'+[0;0;rocket.dcg],...
+                                  'axisrotate',[u(i-1,1), u(i-1,2), u(i-1,3)],theta(i),...
+                                  'translate',-(pos_i(i-1,:)'+[0;0;rocket.dcg]),...
+                                  'translate',pos_i(i-1,:)'));                          
     drawnow
     
 end
-
-for i = 2:length(tspan) 
-    %pause((tspan(i)-tspan(i-1))*stepSize)
-
-%     set(body,'Matrix',makehgtform('translate',pos_i(i-1,:)'+[0;0;rocket.dcg],...
-%                                   'axisrotate',[u(i,1), u(i,2), u(i,3)],theta(i),...
-%                                   'translate',-(pos_i(i-1,:)'-[0;0;rocket.dcg]),...
-%                                   'translate',pos_i(i-1,:)'));
-    set(body,'Matrix',makehgtform('axisrotate',[u(i-1,1), u(i-1,2), u(i-1,3)],theta(i-1)));
-    drawnow
-    
-end
-
--- EquationsOfMotion.m
+++ EquationsOfMotion.m
@@ -1,38 +1,34 @@
 function states_i_dot = EquationsOfMotion(t,states,thrust,tspan,rocket)
 global rho g
-[d,time_index] = min(abs(t-tspan));
-thrust    = thrust(time_index); % Finds the thrust value corresponding to T
 
+[d,time_index] = min(abs(t-tspan)); % This does the same thing as the find() command
+thrust    = thrust(time_index);     % Finds the thrust value corresponding to current time
 
 % External Forces (inertial frame)
+Fg_i     = [0; 0; g*rocket.m]; % force of gravity in the Inertial Frame
+thrust_b = [0; 0; thrust];     % thrust in the Body Fixed Frame
+thrust_i = quatrotate(quatconj(circshift(states(7:10),1)'),thrust_b')';
 
+Fnet_i = Fg_i + thrust_i; % Net force in the Inertial Frame
 
 % External Moments (inertial frame)
-Mnet = [0 0 0];
+Mnet = [0; 0; 0]; % set to 0 for now, but will change when we include aerodynamics
 
-% Euler Equations
+% Omega Matrix to calculate qdot
 omega = [[0            states(13) -states(12) states(11)];
          [-states(13)  0           states(11) states(12)];
          [ states(12) -states(11)  0          states(13)];
          [-states(11) -states(12) -states(13) 0         ]]; % still don't know what this is
-     
-H      = (rocket.I).*states(11:13)';                   % angular momentum
-
-A = rocket.I(1);
-B = rocket.I(2);
-C = rocket.I(3);
-
-wdot_i =  [(B-C)*states(12)*states(13)/A+Mnet(1)/A;
-           (C-A)*states(10)*states(13)/B+Mnet(2)/B;
-           (A-B)*states(10)*states(12)/C+Mnet(3)/C];
 
 % Define states_i_dot as the solutions to the equations of motion
-posdot_i = states(4:6);                       % position
-veldot_i = [0;0;0];%(g*rocket.m)+thrust];         % inertial frame acceleration
-qdot     = 0.5*omega*states(7:10);            % time derivative of quaternions
-% wdot_i   = ((Mnet-cross(states(11:13),H)) ./ (rocket.I))'; % body fixed angular acceleration
+posdot_i = states(4:6);               % inertial frame velocity
+veldot_i = Fnet_i / rocket.m;         % inertial frame acceleration
+qdot     = 0.5*omega*states(7:10);    % time derivative of quaternions
+wdot     = [(rocket.I(2)-rocket.I(3))*states(12)*states(13)/rocket.I(1)+Mnet(1)/rocket.I(1);
+           (rocket.I(3)-rocket.I(1))*states(10)*states(13)/rocket.I(2)+Mnet(2)/rocket.I(2);
+           (rocket.I(1)-rocket.I(2))*states(10)*states(12)/rocket.I(3)+Mnet(3)/rocket.I(3)]; % angular acceleration
 
-states_i_dot = [posdot_i; veldot_i; qdot; wdot_i];
+states_i_dot = [posdot_i; veldot_i; qdot; wdot];
 
 
 
