5193b101e7f5a85e7d1d53f0fa909a43a2ea81f7
ojack123 <ojack123@gmail.com>
2018-10-23 20:35:10 -0400

    Finalized the Animation Function

--- CreateSimulation.m
+++ CreateSimulation.m
@@ -1,11 +1,12 @@
 clc; clear all; close all;
-
-%% Define parameters used throughout the simulation
-global rho g
-rho = 1.225;
-g   = -9.8;
 load rocket;
 load motorCluster;
+%% Define parameters used throughout the simulation
+global rho g Fg_i
+rho  = 1.225;
+g    = 9.8;
+Fg_i = [0; 0; -g*rocket.m]; % force of gravity in the Inertial Frame
+
 % -----------------------------------------------
 % ---------- Reference for state array ---------- 
 % -----------------------------------------------
@@ -17,25 +18,25 @@ load motorCluster;
 %% --------- Define initial states, time span, and resolution here --------
 states = zeros(1,13); % initialize state matrix
 
-yaw   = 0.00001;
-pitch = 0.0;
+initial_yaw   = 0.0;
+initial_pitch = 0.0;
 roll  = 0.0;
-states(7:10) = circshift(angle2quat(yaw,pitch,roll),-1); % Using ZYX for all rotations
-states(11:13)= [0.01 -0.01 0.0];
+states(7:10) = circshift(angle2quat(initial_yaw,initial_pitch,roll),-1); % Using ZYX for all rotations
+states(11:13)= [0.0 0.0 1.0];
 states(1) = 0;
 states(6) = 0.0;
 
-t0       = 0;   % Initial Time
-tf       = 10;  % Final Time
-nip      = 20;  % Number of integration points
+t0       = 0;    % Initial Time
+tf       = 50;   % Final Time
+nip      = 2;    % Number of integration points
 nsteps   = 1000; % Number of steps between t0 and tf ("resolution")
 
 t = t0;         % initialize t
 % -------------------------------------------------------------------------
 %% Define the initial state and time span arrays
-stepSize = 1/nsteps;
+stepSize = tf/nsteps;
 tspan0    = [t0:stepSize:tf]'; % Total time span
-statesIC = states';          % State array used inside the loop
+statesIC = states';            % State array used inside the loop
 
 %% Re-map the thrust data to fit the time span array
 motorCluster = CreateThrustCurves(motorCluster,tspan0);
@@ -43,9 +44,10 @@ motorCluster = CreateThrustCurves(motorCluster,tspan0);
 %% Solve the equations of motion
 options = odeset('JConstant','on', 'RelTol',1e-6, 'AbsTol',1e-6);
 for i = 1:nsteps  
-    t1 = tf*(i-1)/nsteps;
-    t2 = tf*i/nsteps;
+    t1 = stepSize*(i-1);
+    t2 = stepSize*i;
     temp_tspan = t1:(t2-t1)/nip:t2;
+    % can include if statement to stop calling this function
     temp_thrustCurve = CreateThrustCurves(motorCluster,temp_tspan);
     
     [tNew,tempStates] = ode45(@(tNew,statesIC) EquationsOfMotion(tNew,...
@@ -59,9 +61,18 @@ for i = 1:nsteps
     disp((i*100)/nsteps); % display percent completion
 end
 clearvars -except t states stepSize rocket
+%% Display some data
+clc;
+maxSpeed     = norm([max(states(:,4)) max(states(:,5)) max(states(:,6))]);
+apogee       = max(states(:,3));
+disp('Maximum speed');
+disp(maxSpeed);
+disp('Altitude at apogee');
+disp(apogee);
 
 %% Animate the resulting state array
-    whitebg([0 0 0]);
-    zoom = 50; % Distance from camera to the rocket (m)
-    AnimateRocket(t,states,rocket,zoom,'follow'); % 'follow' or 'stationary'
+whitebg([1 1 1]);
+close all;
+zoom = 15; % Distance from camera to the rocket (m)
+AnimateRocket(t,states,rocket,zoom,'stationary'); % 'follow' or 'stationary'
      
--- EquationsOfMotion.m
+++ EquationsOfMotion.m
@@ -1,5 +1,5 @@
 function states_i_dot = EquationsOfMotion(t,states,temp_thrustCurve,tspan,rocket)
-global rho g
+global rho Fg_i
 
 [d,time_index] = min(abs(t-tspan)); % This does the same thing as the find() command
 numMotors = size(temp_thrustCurve); % size is the number of rocket motors
@@ -9,7 +9,6 @@ thrust(i) = temp_thrustCurve(i).thrust(time_index);
 end
 
 % External Forces (inertial frame)
-Fg_i     = [0; 0; g*rocket.m]; % force of gravity in the Inertial Frame
 thrust_b = [zeros(2,numMotors(2)); thrust];     % thrust in the Body Fixed Frame
 
 for i = 1:numMotors(2)
@@ -18,23 +17,23 @@ end
 netThrust_i = sum(thrust_i,2); % sum of all the thrusts from each motor
 
 Fnet_i = Fg_i + netThrust_i; % Net force in the Inertial Frame
+%!!! Include the next part
 
 % External Moments (inertial frame)
 Mnet = [0; 0; 0]; % set to 0 for now, but will change when we include aerodynamics
 
-% Omega Matrix to calculate qdot
-omega = [[0            states(13) -states(12) states(11)];
+% Omega Matrix and angular momentum to calculate qdot
+Omega = [[0            states(13) -states(12) states(11)];
          [-states(13)  0           states(11) states(12)];
          [ states(12) -states(11)  0          states(13)];
-         [-states(11) -states(12) -states(13) 0         ]]; % still don't know what this is
+         [-states(11) -states(12) -states(13) 0         ]];
+H     = (rocket.I)'.*states(11:13);                   % angular momentum
 
 % Define states_i_dot as the solutions to the equations of motion
 posdot_i = states(4:6);               % inertial frame velocity
 veldot_i = Fnet_i / rocket.m;         % inertial frame acceleration
-qdot     = 0.5*omega*states(7:10);    % time derivative of quaternions
-wdot     = [(rocket.I(2)-rocket.I(3))*states(12)*states(13)/rocket.I(1)+Mnet(1)/rocket.I(1);
-            (rocket.I(3)-rocket.I(1))*states(10)*states(13)/rocket.I(2)+Mnet(2)/rocket.I(2);
-            (rocket.I(1)-rocket.I(2))*states(10)*states(12)/rocket.I(3)+Mnet(3)/rocket.I(3)]; % angular acceleration
+qdot     = 0.5*Omega*states(7:10);    % time derivative of quaternions
+wdot     = ((Mnet-cross(states(11:13),H))' ./ (rocket.I))'; % angular acceleration
 
 states_i_dot = [posdot_i; veldot_i; qdot; wdot];
 
