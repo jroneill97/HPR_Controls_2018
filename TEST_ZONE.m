d7e00570509d16498b4cc1a8c6bd36efd627ae55
alvarez231 <sokkerplayer23@gmail.com>
2019-01-31 16:33:36 -0500

    Added matrices to compute F and then the proper Jacobian values
    -Alex

--- TEST_ZONE.m
+++ TEST_ZONE.m
@@ -1,58 +1,49 @@
 %% MQP work for Rocket Controller
 clc; clear all; close all;
-% 
-% syms u v w Fx Fy Fz m q1 q2 q3 q4 wx wy wz Mx My Mz Ixx Iyy Izz
-% % define matrix groups
-% q = [q1; q2; q3; q4];
-% w0 = [wx ;wy; wz];
-% M = [Mx; My; Mz];
-% I = [Ixx 0 0; 0 Iyy 0; 0 0 Izz];
-% H = [Ixx*wx; Iyy*wy; Izz*wz];
-% % define matrix helper formulas
-% w_x = [0 -wz wy; wz 0 -wx; -wy wx 0];
-% O = [-w_x w0; -w0' 0];
-% q_dot = .5*O*q;
-% w_dot = I\(M-cross(w0,H));
-% 
-% x_dot = [u; v; w; Fx/m; Fy/m; Fz/m; q_dot; w_dot];
-% disp(x_dot);
-% 
-% vars = [u; v; w; Fx; Fy; Fz; q1; q2; q3; q4; wx; wy; wz];
-% J_x = jacobian(x_dot,vars);
-% disp(J_x);
 
-%% 
+syms x y z u v w T m g rho Cl Cd A_fin A_front q1 q2 q3 q4 wx wy wz Mx My Mz Ixx Iyy Izz
+% define matrix groups
+q = [q1; q2; q3; q4];
+w0 = [wx ;wy; wz];
+M = [Mx; My; Mz];
+I = [Ixx 0 0; 0 Iyy 0; 0 0 Izz];
+H = [Ixx*wx; Iyy*wy; Izz*wz];
+vel = (u^2+v^2+w^2);
+% define matrix helper formulas
+w_x = [0 -wz wy; wz 0 -wx; -wy wx 0];
+O = [-w_x w0; -w0' 0];
+q_dot = .5*O*q;
+w_dot = I\(M-cross(w0,H));
 
-% DCM for velocity fixed to body fixed frame of reference
-RvB = [[      1/(v^2/w^2 + 1)^(1/2), -v^2/(w*(v^2/w^2 + 1)^(1/2)*(u^2 + v^2 + w^2)^(1/2)), (v*(1 - v^2/(u^2 + v^2 + w^2))^(1/2))/(w*(v^2/w^2 + 1)^(1/2))];
-       [                          0,                    (1 - v^2/(u^2 + v^2 + w^2))^(1/2),                                     v/(u^2 + v^2 + w^2)^(1/2)];
-       [ -v/(w*(v^2/w^2 + 1)^(1/2)),     -v/((v^2/w^2 + 1)^(1/2)*(u^2 + v^2 + w^2)^(1/2)),         (1 - v^2/(u^2 + v^2 + w^2))^(1/2)/(v^2/w^2 + 1)^(1/2)]];
+%Aerodynamic Force/Moment
+a = -atan(v/w);
+b = asin(v/sqrt(vel));
+T_bi = [2*(q2*q4+q1*q3)*T; 2*(q3*q4-q1*q2)*T; (q1^2-q2^2-q3^2+q4^2)*T];
+F_ad = [.5*rho*sqrt(vel)*Cl*sin(a)*A_fin;...
+        .5*rho*sqrt(vel)*Cl*sin(b)*A_fin;...
+        .5*rho*sqrt(vel)*Cd*A_front]';
+%DCM for velocity fixed to body fixed frame of reference
+R_vb(1,:) = [1/sqrt(v^2/w^2+1) -v^2/(w*sqrt(v^2/w^2+1)*sqrt(vel))...
+            v*(1-(v^2/sqrt(vel)))/(w*sqrt(v^2/w^2+1))];
+R_vb(2,:) = [0 sqrt(1-v^2/vel) v/sqrt(vel)];...
+R_vb(3,:) = [-v/(w*sqrt(v^2/w^2 + 1)) -v/(sqrt(v^2/w^2 + 1)*sqrt(vel))...
+            (1-v^2/sqrt(vel))/sqrt(v^2/w^2 + 1)];
+R_bi = [q1^2-q2^2-q3^2+q4^2 2*(q1*q2+q3*q4) 2*(q1*q3-q2*q4);...
+        2*(q2*q1-q3*q4) -q1^2+q2^2-q3^2+q4^2 2*(q2*q3+q1*q4);...
+        2*(q3*q1+q2*q4) 2*(q3*q2-q1*q4) -q1^2-q2^2+q3^2+q4^2];
+F_aero = F_ad*R_vb*R_bi;
+F = T_bi+[0;0;-m*g]+F_aero';
+%disp(F);
 
+x_dot = [u; v; w; F(1)/m; F(2)/m; F(3)/m; q_dot; w_dot]; %13x1 column vector
+%disp(x_dot);
 
+vars = [u; v; w; q1; q2; q3; q4; wx; wy; wz];
+A = jacobian(x_dot,vars); %13x13 matrix
+%disp(A);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+C = [0 0 1 0 0 0 0 0 0 0 0 0 0;...
+    0 0 0 0 0 0 0 0 0 0 1 0 0;...
+    0 0 0 0 0 0 0 0 0 0 0 1 0;...
+    0 0 0 0 0 0 0 0 0 0 0 0 1]*x_dot;
+%disp(C);
