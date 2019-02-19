76c189d18d94d5c1375200bfbf72972ff66a5e1f
alvarez231 <sokkerplayer23@gmail.com>
2019-02-19 13:50:02 -0500

    messed around -alex

--- TEST_ZONE.m
+++ TEST_ZONE.m
@@ -21,14 +21,9 @@ I = [Ixx 0 0; 0 Iyy 0; 0 0 Izz];
 H = [Ixx*wx; Iyy*wy; Izz*wz];
 
 % define matrix helper formulas
-% w_x = [0 -wz wy; wz 0 -wx; -wy wx 0];
-% O = [-w_x w0; -w0' 0];
-O           = [[0            wz -wy wx];
-               [-wz  0           wx wy];
-               [ wy -wx  0          wz];
-               [-wx -wy -wz 0         ]];
+w_x = [0 -wz wy; wz 0 -wx; -wy wx 0];
+O = [-w_x w0; -w0' 0];
 q_dot = 0.5*O*q;
-
 
 %Aerodynamic Force/Moment
 a = -atan(v/w);
@@ -37,6 +32,7 @@ T_bi = [2*(q2*q4+q1*q3)*T; 2*(q3*q4-q1*q2)*T; (q1^2-q2^2-q3^2+q4^2)*T];
 F_ad = [0.5*rho*speed*Cl*A_side*((v/w)/sqrt((v^2/w)+1))*2*pi*atan(v/w);...
         0.5*rho*speed*Cl*A_side*(v/speed)*2*pi*atan(v/w);...
         0.5*rho*speed^2*Cd*A_front]';
+
 %DCM for velocity fixed to body fixed frame of reference
 R_vb(1,:) = [1/sqrt(v^2/w^2+1) -v^2/(w*sqrt(v^2/w^2+1)*sqrt(speed))...
             v*(1-(v^2/sqrt(speed)))/(w*sqrt(v^2/w^2+1))];
@@ -47,7 +43,8 @@ R_bi = [q1^2-q2^2-q3^2+q4^2 2*(q1*q2+q3*q4) 2*(q1*q3-q2*q4);...
         2*(q2*q1-q3*q4) -q1^2+q2^2-q3^2+q4^2 2*(q2*q3+q1*q4);...
         2*(q3*q1+q2*q4) 2*(q3*q2-q1*q4) -q1^2-q2^2+q3^2+q4^2];
 F_aero = F_ad*R_vb*R_bi;
-F = T_bi+[0;0;-m*g]+[F_aero(1) F_aero(2) F_aero(3)];
+F = T_bi+[0;0;-m*g]+[F_aero(1) F_aero(2) F_aero(3)]';
+%disp(F);
 
 % Rotate the distance from CP to CG into the inertial frame
 cp_vector = [0 0 dcp];
@@ -61,18 +58,18 @@ M_fins_i = M_fins_b*R_bi;
 M_aero   = cross(F_aero, cp2cg_i);
 
 M=M_fins_i+M_aero;
-w_dot = I\(M-cross(w0,H));
-%disp(F);
+%w_dot = I\(M-cross(w0,H));
+w_dot = I'\(M'-cross(w0,H)); %this fixes size error
 
-% x_dot = [u; v; w; F(1)/m; F(2)/m; F(3)/m; q_dot; w_dot]; %13x1 column vector
+x_dot = [u; v; w; F(1)/m; F(2)/m; F(3)/m; q_dot; w_dot]; %13x1 column vector
 %disp(x_dot);
 
 vars = [u; v; w; q1; q2; q3; q4; wx; wy; wz];
-% A = jacobian(x_dot,vars); %13x13 matrix
+A = jacobian(x_dot,vars); %13x13 matrix
 %disp(A);
 B = [zeros(11,4);jacobian(M_fins_i,[u1,u2,u3,u4])];
 % C = [0 0 1 0 0 0 0 0 0 0 0 0 0;...
-    0 0 0 0 0 0 0 0 0 0 1 0 0;...
-    0 0 0 0 0 0 0 0 0 0 0 1 0;...
-    0 0 0 0 0 0 0 0 0 0 0 0 1]*x_dot;
+    %0 0 0 0 0 0 0 0 0 0 1 0 0;...
+    %0 0 0 0 0 0 0 0 0 0 0 1 0;...
+    %0 0 0 0 0 0 0 0 0 0 0 0 1]*x_dot;
 %disp(C);
